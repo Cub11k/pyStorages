4532b0348755c3e9c86a7e8d6c69bef09f9efc70
Cub11k <ostashenkokd@gmail.com>
2022-12-24 00:55:10 +0200

    Change default file paths

--- src/storages/json_storage.py
+++ src/storages/json_storage.py
@@ -9,14 +9,14 @@ def create_dir(file_path):
     dirs = file_path.rsplit('/', maxsplit=1)[0]
     os.makedirs(dirs, exist_ok=True)
     if not os.path.isfile(file_path):
-        with open(file_path, 'wb') as file:
+        with open(file_path, 'w') as file:
             json.dump({}, file)
 
 
 class JSONStorage(BaseStorage):
     def __init__(self, file_path: Optional[str]):
         super().__init__()
-        self.file_path = file_path if file_path is not None else "../.storages/data.json"
+        self.file_path = file_path if file_path is not None else "./.storages/data.json"
         create_dir(self.file_path)
         self.load()
 
