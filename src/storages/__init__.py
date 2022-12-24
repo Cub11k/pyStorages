303ba6ee6aa0516a5828095025b8ca3b11e43d7f
Cub11k <ostashenkokd@gmail.com>
2022-12-24 18:20:11 +0200

    Add custom redis data key

--- src/storages/__init__.py
+++ src/storages/__init__.py
@@ -12,7 +12,10 @@ class StorageType(Enum):
     redis = "redis"
 
 
-def load_storage(storage_type: StorageType, file_path: Optional[str] = None, redis_url: Optional[str] = None):
+def load_storage(storage_type: StorageType,
+                 file_path: Optional[str] = None,
+                 redis_url: Optional[str] = None,
+                 redis_data_key: Optional[str] = None):
     storage = {
         StorageType.pickle: PickleStorage,
         StorageType.json: JSONStorage,
@@ -21,7 +24,7 @@ def load_storage(storage_type: StorageType, file_path: Optional[str] = None, red
     if storage_type in [StorageType.pickle, StorageType.json]:
         return storage[storage_type](file_path)
     elif storage_type == StorageType.redis:
-        return storage[storage_type](redis_url)
+        return storage[storage_type](redis_url, redis_data_key)
 
 
 Storage = Union[PickleStorage, JSONStorage, RedisStorage]
